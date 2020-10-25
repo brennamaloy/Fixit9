a8227bccd15f89e07450e4e858a5794a9d2a424b
brennamaloy <bkm91466@creighton.edu>
2020-10-25 11:58:54 -0500

    Update describeYou.js

--- forms/describeYou/describeYou.js
+++ forms/describeYou/describeYou.js
@@ -1,5 +1,9 @@
+rbnPersonality.onclick=function(){
+  let userChoice = $("input[name=rbnPersonality]:checked").prop("value")
+  lblType.hidden = false
+  lblType.textContent = (`I would agree that you are a '${userChoice}' person too!`)
+}
 
-
-
-
-
+fEPage.onclick=function(){
+  ChangeForm(favExercises)
+}
